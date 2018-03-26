9b0cd68cc831f99df620728ef0e0ec2e89c9d388
adm <adm@nadev.info>
2018-03-26 22:17:23 +0300

    lesson8

--- lesson8/index.php
+++ lesson8/index.php
@@ -4,11 +4,38 @@
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     </head>
     <body>
+        
+        <h2>News</h2> 
+        
+        <?php
+            require_once('news.php');
+        
+            $newsFileName = __DIR__.'/news.dat';
+            if (file_exists($newsFileName))
+            {
+                $newsFileContents = file_get_contents($newsFileName);
+                $newsArray = explode (PHP_EOL, $newsFileContents);
+                
+                foreach ($newsArray as $newsStr)
+                {
+                    $news = unserialize($newsStr);
+                    
+                    if (is_a($news, 'News'))
+                    {
+                        echo $news->getFullText();
+                    }
+                    
+                    echo '<br/><br/>';
+                }
+            }                
+        ?>
+        
         <h2> Main test dashboard  </h2>
         <form name="inputForm" method="GET">
             <fieldset>
                 <br/>
                 <br/>
+                <input type="submit" name="newsupload" value="Upload news"/>
                 <input type="submit" name="upload" value="Upload test"/>
                 <input type="submit" name="pass" value="Pass test"/>
             </fieldset>
@@ -19,6 +46,7 @@
 <?php
 
     require_once('functions.php');
+    require_once('news.php');
     
     if (isset($_GET['upload'])) 
     {
@@ -27,5 +55,9 @@
     else if (isset($_GET['pass']))
     {
         redirect('list');
+    }
+    else if (isset($_GET['newsupload']))
+    {
+        redirect('newsupload');
     }
 ?>
