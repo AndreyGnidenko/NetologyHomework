0872e3e72714aa093408691be2adbd308b880593
adm <adm@nadev.info>
2018-04-03 03:04:50 +0300

    lesson7

---
+++ lesson7/displayResult.php
@@ -0,0 +1,20 @@
+<?php
+
+$image = imagecreatefrompng('certificate.png');
+
+$blue = imagecolorallocate($image, 0, 0, 255);
+
+$certHeader = 'User '.$_GET['userName']. ' has passed the test '.$_GET['testName'];
+$certResult = 'Test result is '.$_GET['score']. ' correct answers out of '.$_GET['maxScore'];
+
+imagettftext($image, 36, 0, 500, 300, $blue, __DIR__.'/vera.ttf', $certHeader);
+imagettftext($image, 36, 0, 500, 500, $blue, __DIR__.'/vera.ttf', $certResult); 
+
+var_dump($image);
+                
+header('Content-type: image/png');
+
+imagepng($image, NULL, 6, PNG_ALL_FILTERS);
+imagedestroy($image);
+
+?>
