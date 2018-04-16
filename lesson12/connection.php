5b785333027f1ff2df37b1351435bb55e0996c30
adm <adm@nadev.info>
2018-04-16 03:27:34 +0300

    Adding delegation display and task operations

--- lesson12/connection.php
+++ lesson12/connection.php
@@ -5,6 +5,11 @@ class DatabaseConnection
     private $dsn = 'mysql:dbname=tasks;host=localhost;charset=utf8';
     private $user = 'root';
     private $password = '';
+    
+    //private $dsn = 'mysql:dbname=global;host=localhost;charset=utf8';
+    //private $user = 'gnidenko';
+    //private $password = 'neto1689';
+    
     private $dbh;
     
     public function __construct ()
@@ -68,9 +73,23 @@ class DatabaseConnection
     
     public function getUserTasks ($userName, $sortField)
     {
+        $columnNames = array ('id', 'description', 'date_added', 'is_done');
+        
+        $sqlQuery = 'SELECT t.'.implode(', t.', $columnNames). ', u.login creator, u2.login assignee FROM task t INNER JOIN user u ON t.user_id = u.id INNER JOIN user u2 ON t.assigned_user_id = u2.id WHERE u.login=:userName ORDER BY '.$sortField;
+        $sqlStatement = $this->dbh->prepare($sqlQuery);
+    
+        $insertValues = array();
+        $insertValues['userName'] = $userName;
+            
+        $sqlStatement->execute($insertValues);
+        return $sqlStatement->fetchAll(PDO::FETCH_ASSOC);
+    }
+    
+    public function getAssignedTasks ($userName, $sortField)
+    {
         $columnNames = array ('description', 'date_added', 'is_done');
         
-        $sqlQuery = 'SELECT t.'.implode(', t.', $columnNames). ', u.login creator, u2.login assignee FROM task t INNER JOIN user u ON t.user_id = u.id INNER JOIN user u2 ON t.assigned_user_id = u2.id WHERE u.login=:userName ORDER BY :sortField';
+        $sqlQuery = 'SELECT t.'.implode(', t.', $columnNames). ', u.login creator, u2.login assignee FROM task t INNER JOIN user u ON t.user_id = u.id INNER JOIN user u2 ON t.assigned_user_id = u2.id WHERE u2.login=:userName AND t.user_id!=t.assigned_user_id ORDER BY :sortField';
         $sqlStatement = $this->dbh->prepare($sqlQuery);
     
         $insertValues = array();
@@ -80,6 +99,7 @@ class DatabaseConnection
         $sqlStatement->execute($insertValues);
         return $sqlStatement->fetchAll(PDO::FETCH_ASSOC);
     }
+    
     
     public function getUsers ()
     {
@@ -97,6 +117,40 @@ class DatabaseConnection
         $insertValues['taskId'] = $taskId;
         $insertValues['assignee'] = $assignee;
         
+        $sqlStatement = $this->dbh->prepare($sqlQuery);
+        $sqlStatement->execute($insertValues);
+    }
+    
+    public function renameTask($taskId, $newDescription)
+    {
+        $sqlQuery = 'UPDATE task SET description=:desc WHERE id=:taskId';
+        
+        $insertValues = array();
+        $insertValues['taskId'] = $taskId;
+        $insertValues['desc'] = $newDescription;
+        
+        $sqlStatement = $this->dbh->prepare($sqlQuery);
+        $sqlStatement->execute($insertValues);
+    }
+    
+    public function resolveTask ($taskId)
+    {
+        $sqlQuery = 'UPDATE task SET is_done=1 WHERE id=:taskId';
+        
+        $insertValues = array();
+        $insertValues['taskId'] = $taskId;
+        
+        $sqlStatement = $this->dbh->prepare($sqlQuery);
+        $sqlStatement->execute($insertValues);
+    }
+    
+    public function deleteTask ($taskId)
+    {
+        $sqlQuery = 'DELETE FROM task WHERE id=:taskId';
+        
+        $insertValues = array();
+        $insertValues['taskId'] = $taskId;
+         
         $sqlStatement = $this->dbh->prepare($sqlQuery);
         $sqlStatement->execute($insertValues);
     }
--- lesson12/tasks.php
+++ lesson12/tasks.php
@@ -26,6 +26,37 @@
     table th {
         background: #eee;
     }
+    
+    .frm-link {
+        display:inline;
+        margin:4;
+        padding:0;
+    }
+    
+    .btn-link{
+          border:none;
+          outline:none;
+          background:none;
+          cursor:pointer;
+          color:#EE0000;
+          padding:0;
+          text-decoration:underline;
+          font-family:inherit;
+          font-size:inherit;
+    }
+    
+    .btn-link-blue{
+          border:none;
+          outline:none;
+          background:none;
+          cursor:pointer;
+          color:#0000EE;
+          padding:0;
+          text-decoration:underline;
+          font-family:inherit;
+          font-size:inherit;
+    }
+    
 </style>
 
 <html lang="ru">
@@ -58,54 +89,176 @@
         
         <?php
         
+            function displayTaskCells ($taskRecord)
+            {
+                foreach ($taskRecord as $internalColumnName=>$taskRecordColumn)
+                {
+                    if ($internalColumnName !== 'id')
+                    {
+                        $columnVal = $taskRecordColumn;
+                        if ($internalColumnName === 'is_done')
+                        {
+                            $columnVal = $taskRecordColumn ? 'Complete' : 'In progress';
+                        }
+                        echo '<td>', $columnVal, '</td>';
+                    }
+                }
+            }
+            
+            function displayTaskOperations ($taskId)
+            {
+                $operations = array ('Modify'=>'green');
+                
+                $strOperations = '<td>';
+                
+                foreach ($operations as $operationName=>$color)
+                {
+                    $strOperations.='<a href="?taskId='.$taskId.'&operation='.$operationName.'" style="color: '.$color.'">'.$operationName.'</a>&ensp;'; 
+                }
+                
+                $strOperations.='<form method="post" class="frm-link">';
+                $strOperations.='<button type="submit" name="resolve" value="resolve" class="btn-link-blue"> Resolve </button>';
+                $strOperations.='<input type="hidden" name="taskId" value="'.$taskId.'"/>';
+                $strOperations.='</form>';
+                
+                $strOperations.='<form method="post" class="frm-link">';
+                $strOperations.='<button type="submit" name="delete" value="delete" class="btn-link">Delete</button>';
+                $strOperations.='<input type="hidden" name="taskId" value="'.$taskId.'"/>';
+                $strOperations.='</form>';
+                
+                $strOperations.='</td>';
+                echo $strOperations;
+            }
+            
+            function displayChangeDescForm ($taskId)
+            {
+                echo '<form name="rename" method="POST"><fieldset><legend>New description for task</legend>';
+                echo '<input type="text" name="newDesc" placeholder="New description"/><br/>';
+                echo '<input type="hidden" name="taskId" value="'.$taskId.'"/>';
+                echo '<input type="submit" name="rename" value="Change description"/>';
+                echo '</fieldset></form>';
+            }
+            
+            
             $columnNames = array ('description' => 'Description', 'date_added'=>'Time', 'is_done'=>'Status', 'creator'=>'Created by', 'assignee'=>'Assigned to');
             
             $dbConnection = new DatabaseConnection;
+            
+            if (empty($_POST) && isset($_GET['operation']) && isset($_GET['taskId']))
+            {
+                $operation = $_GET['operation'];
+                if ($operation === 'Modify')
+                {
+                    displayChangeDescForm($_GET['taskId']);
+                }
+            }
             
             if (isset($_POST['addTask']) && !empty($_POST['description']) ) 
             {
                 $dbConnection->addTask($userName, $_POST['description']);
             }
             
+            if (isset($_POST['delegate']) )
+            {
+                $assignedId = $_POST['assigned_id'];
+                
+                $regexMatches = array();
+                
+                preg_match('^task([0-9]*)_user([0-9]*)$^', $assignedId, $regexMatches);
+                
+                $taskId = (int)$regexMatches[1];
+                $assignee = (int)$regexMatches[2];
+                
+                $dbConnection->assignTask($taskId, $assignee);
+            }
+            
+            if (isset($_POST['rename']) && isset($_POST['taskId']) && isset($_POST['newDesc']) )
+            {
+                $taskId = $_POST['taskId'];
+                
+                $dbConnection->renameTask($taskId, $_POST['newDesc']);
+            } 
+            
+            if (isset($_POST['resolve']) && isset($_POST['taskId']) )
+            {
+                $taskId = $_POST['taskId'];
+                $dbConnection->resolveTask($taskId);
+            }  
+
+            if (isset($_POST['delete']) && isset($_POST['taskId']) )
+            {
+                $taskId = $_POST['taskId'];
+                $dbConnection->deleteTask($taskId);
+            }                
+            
             $sortField = isset($_POST['doSort']) ? $_POST['sortBy'] : 'date_added';
         
             $tasks = $dbConnection->getUserTasks($userName, $sortField);
             $userRecords = $dbConnection->getUsers();
 
-            echo '<table name="Tasks">';
-           
-            echo '<tr>';
-            foreach($columnNames as $columnName)
             {
-                echo '<th>', $columnName, '</th>';
-            }
-            echo '<th>Delegation<th/>';
-            echo '</tr>';
+                echo '<h2>Tasks created by me</h2>';
             
-            foreach ($tasks as $taskRecord)
-            {
+                echo '<table name="MyTasks">';
+               
                 echo '<tr>';
-                foreach ($taskRecord as $internalColumnName=>$taskRecordColumn)
+                foreach($columnNames as $columnName)
                 {
-                    $columnVal = $taskRecordColumn;
-                    if ($internalColumnName === 'is_done')
-                    {
-                        $columnVal = $taskRecordColumn ? 'Complete' : 'In progress';
-                    }
-                    echo '<td>', $columnVal, '</td>';
+                    echo '<th>', $columnName, '</th>';
                 }
-                echo '<td> <form method=\'POST\'> <select name=\'assigned_id\'>';
-                
-                foreach($userRecords as $userRecord)
-                {
-                    echo '<option value=', $userRecord['id'], '>', $userRecord['userName'], '</option>';
-                }
-                
-                echo '</select> <input type=\'submit\' name=\'delegate\' value=\'Delegate\'> </form> </td>';
+                echo '<th>Actions</th>';
+                echo '<th>Delegation</th>';
                 echo '</tr>';
+                
+                foreach ($tasks as $taskRecord)
+                {
+                    echo '<tr>';
+                    
+                    $taskId = $taskRecord['id'];
+                    
+                    displayTaskCells($taskRecord);
+
+                    displayTaskOperations($taskId);
+
+                    echo '<td> <form method=\'POST\'> <select name=\'assigned_id\'>';
+                    
+                    foreach($userRecords as $userRecord)
+                    {
+                        echo '<option value=task', $taskId, '_user', $userRecord['id'], '>', $userRecord['userName'], '</option>';
+                    }
+                    
+                    echo '</select> <input type=\'submit\' name=\'delegate\' value=\'Delegate\'> </form> </td>';
+                    echo '</tr>';
+                }
+                
+                echo '</table>';
             }
             
-            echo '</table>';
+            {
+                echo '<h2>Tasks assigned to me by others</h2>';
+                
+                echo '<table name="AssignedTasks">';
+                
+                echo '<tr>';
+                foreach($columnNames as $columnName)
+                {
+                    echo '<th>', $columnName, '</th>';
+                }
+                echo '</tr>';
+                
+                $assignedTasks = $dbConnection->getAssignedTasks($userName, $sortField);
+                
+                foreach ($assignedTasks as $assignedTaskRecord)
+                {
+                    echo '<tr>';
+                    
+                    displayTaskCells($assignedTaskRecord);
+                    
+                    echo '</tr>';
+                }
+                echo '</table>';
+            }
+
         ?>
         
         <br/><br/>
